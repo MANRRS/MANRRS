c17d02e75a39f0da75bb0a7e7bbce76cc785308f
stoned-ape <samuraiheadhunter@gmail.com>
2022-03-28 07:02:10 -0500

    update script

--- curl.sh
+++ curl.sh
@@ -1,4 +1,4 @@
-curl 'https://myaccount.google.com/?utm_source=sign_in_no_continue&pli=1' \
+curl -I 'https://myaccount.google.com/?utm_source=sign_in_no_continue&pli=1' \
   -H 'authority: myaccount.google.com' \
   -H 'upgrade-insecure-requests: 1' \
   -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36' \
